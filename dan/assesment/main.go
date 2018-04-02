6b25ab9e4350776c159489433e2aebcfddc0777e
Dan <danslutzky@gmail.com>
2018-04-02 20:07:37 +0200

    another test

--- dan/assesment/main.go
+++ dan/assesment/main.go
@@ -7,7 +7,7 @@ func main() {
 	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	for _, slice := range slice {
-		//fmt.Println(i, slice)
+		//fmt.Println(i, slice) ////// 
 		if slice%2 == 0 {
 			fmt.Println(slice, "is odd number")
 		}
